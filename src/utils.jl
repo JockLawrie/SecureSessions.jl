e3c354a8db708b513ed8bfb1d648393ee58bb903
Jock Lawrie <jock.lawrie@gmail.com>
2015-11-11 13:18:15 +1100

    Added basic username validation.
    Signed-off-by: Jock Lawrie <jock.lawrie@gmail.com>

--- src/utils.jl
+++ src/utils.jl
@@ -18,3 +18,24 @@ Takes 13 characters (valid until some time around the year 2287).
 function get_timestamp()
     1000 * convert(Int, Dates.datetime2unix(now()))
 end
+
+
+"""
+Returns true if username adheres to the following formatting rules:
+1) length(username) <= 20
+2) username contains none of the following tags:
+       "<script>", "<link>", "<img>", "<iframe>", "<object>"
+"""
+function username_is_permissible(username)
+    result = length(username) <= 20
+    if result
+        bad_tags = ["<script>", "<link>", "<img>", "<iframe>", "<object>"]
+        for tag in bad_tags
+            if contains(username, tag)
+                 result = false
+                 break
+            end
+        end
+    end
+    result
+end
